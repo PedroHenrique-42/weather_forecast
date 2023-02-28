d9b243515321eb0d3a588064e00c71d3b0279d17
Pedro <phlferezin03@gmail.com>
2023-02-28 09:24:31 -0300

    update

--- test/main_screen_test.dart
+++ test/main_screen_test.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
-import 'package:weather_forecast/components/current_weather_informations/weather_informations_widget.dart';
 import 'package:weather_forecast/providers/weather_data_provider.dart';
 import 'package:weather_forecast/screens/main_screen.dart';
-import 'package:weather_forecast/utils/show_dialog.dart';
 
 void main() {
   testWidgets("My screen has a text 'Pesquise um local'", (tester) async {
@@ -19,24 +17,5 @@ void main() {
 
     final pesquiseFinder = find.text("Pesquise um local");
     expect(pesquiseFinder, findsOneWidget);
-  });
-
-  testWidgets("Finds a search bar", (tester) async {
-    await tester.pumpWidget(
-      ChangeNotifierProvider(
-        create: (changeNotifierContext) {
-          return WeatherDataProvider();
-        },
-        child: const MaterialApp(
-          home: MainScreen(),
-        ),
-      ),
-    );
-
-    expect(find.byType(WeatherInformationsWidget), findsOneWidget);
-
-    await tester.pumpWidget(const WeatherInformationsWidget());
-
-    expect(find.byType(FutureBuilder), findsOneWidget);
   });
 }
